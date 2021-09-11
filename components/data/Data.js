cc2e14845095d7c5b73522c665ee54ddca778d25
MADNI01 <majdkot0@gmail.com>
2021-09-11 05:44:27 +0300

    stating

---
+++ components/data/Data.js
@@ -0,0 +1,11 @@
+const Data = [
+  { name: "Annie Leonhart", img: "annie.jpg" },
+  { name: "Reiner Braun", img: "reiner.jpg" },
+  { name: "Bertolt Hoover", img: "bert.jpg" },
+  { name: "mikasa ackerman", img: "mikasa.jpg" },
+  { name: "eren yeager", img: "eren.jpg" },
+  { name: "Levi Ackerman", img: "levi.jpg" },
+  { name: "Armin Arlert", img: "armin.jpg" },
+  { name: "Sasha Braus", img: "sasha.jpg" },
+];
+export default Data
---
+++ pages/index.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useRef, useState } from "react";
+import Head from "next/head";
+import classes from "../styles/Home.module.css";
+import Container from "../components/container/Container";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Data from "../components/data/Data";
+
+export default function Home() {
+    /* get window dimensions */
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+    useEffect(() => {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+  
+      window.addEventListener("resize", handleResize);
+  
+      handleResize();
+  
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    /* get window dimensions */
+
+  const settings1 = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+    variableWidth: true,
+    draggable: windowSize.width > 1000 ? false : true,
+    waitForAnimate:false,
+    arrows:false
+  };
+  const slider1 = useRef();
+
+
+
+  return (
+    <>
+      <div className={classes.home}>
+        <Container className={classes.container}>
+          <div className={classes.nav}>
+            <img src='logo.png'/>
+          </div>
+          <div className={classes.names}>
+            {Data.map((item, n) => (
+              <h2
+                onMouseEnter={() => {
+                  slider1.current.slickGoTo(n);
+                }}
+
+                
+              >
+                {item.name}
+              </h2>
+            ))}
+          </div>
+        </Container>
+        <div className={classes.content}>
+          <Slider {...settings1} ref={(slider) => (slider1.current = slider)}>
+            {Data.map((item) => (
+              <div className={classes.card}>
+                <div className={classes.box}>
+                  <img onClick={()=>{console.log(item.name)}} src={item.img} />
+                  {windowSize.width < 1000 ? <h2>{item.name}</h2> : null}
+                </div>
+                <div className={classes.text}></div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
