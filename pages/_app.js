07573df626adf0953f4c6d87a04d482cdbd031f9
MADNI01 <majdkot0@gmail.com>
2021-09-14 03:46:09 +0300

    s

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 function MyApp({ Component, pageProps, router }) {
   
   return (
 
       <AnimatePresence initial={false} exitBeforeEnter>
-        <Component {...pageProps} key={router.route}  />
+        <motion.div key={router.route}>
+        <Component {...pageProps}   />
+        </motion.div>
       </AnimatePresence>
 
   );
