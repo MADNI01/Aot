067c4785b0d361b4ff411d20dda06feb6c40dc1d
MADNI01 <majdkot0@gmail.com>
2021-09-23 20:57:34 +0300

    d

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ function MyApp({ Component, pageProps, router }) {
   
   return (
 
-      <AnimatePresence initial={false} /* exitBeforeEnter */>
-        <motion.div key={router.route}>
+      <AnimatePresence initial={false} exitBeforeEnter>
+        <motion.div /* key={router.route} */>
         <Component {...pageProps}   />
         </motion.div>
       </AnimatePresence>
