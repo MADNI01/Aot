35e2d2d06f66421cafd995ef7cec53d0b6f998f7
MADNI01 <majdkot0@gmail.com>
2021-09-23 20:02:05 +0300

    d

--- components/aniSection/AniSection.js
+++ components/aniSection/AniSection.js
@@ -5,7 +5,6 @@ import Data from "../data/Data";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import progressiveImage from 'react-progressive-image'
 const AniSection = () => {
     /* get window dimensions */
     const [windowSize, setWindowSize] = useState({
@@ -39,10 +38,18 @@ const AniSection = () => {
 
   const transition = { duration: .6, ease: [0.43, 0.13, 0.23, 0.96] };
 
+
+
+
   return (
     <div className={classes.section}>
       <Container>
         <Nav />
+        <motion.div className={classes.name} initial={{y:300,x:'50%',opacity:0}} animate={{y:'50%',x:'50%',opacity:1}} transition={{...transition,delay:2.6,duration:1}}>
+          <span>
+            {data[index]?.name}
+          </span>
+        </motion.div>
         <motion.div className={classes.content}initial={{y:'49%',x:'50%'}} animate={{bottom:0,y:0,x:'50%',width:'100%'}} transition={{...transition,duration: 1.2,delay:1.7}}>
           <motion.div className={classes.card} animate={{width:0}} transition={{delay:1.3}}>
             <div className={classes.box} >
